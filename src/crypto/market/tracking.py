fba46d359093eaa2d46f8196e7e852ced6ca5ea3
empty <empty@emptys-MacBook-Pro.local>
2025-09-11 10:11:53 +0200

    more aggressive skewing

--- src/crypto/market/tracking.py
+++ src/crypto/market/tracking.py
@@ -81,24 +81,26 @@ async def manage_orders(mexc_client: MexcClient, kucoin_client: KucoinClient, da
         ask_shift -= MEXC_TICK_SIZE
         bid_shift -= MEXC_TICK_SIZE
 
-    if full_rmv_balance - INVENTORY_BALANCE > 37_500: # we are long
-        ask_shift -= MEXC_TICK_SIZE
-        bid_shift -= MEXC_TICK_SIZE
-    elif full_rmv_balance - INVENTORY_BALANCE < -37_500: # we are short
-        bid_shift += MEXC_TICK_SIZE
-        ask_shift += MEXC_TICK_SIZE
-    elif full_rmv_balance - INVENTORY_BALANCE > 75_000: # we are long
-        ask_shift -= 2 * MEXC_TICK_SIZE
-        bid_shift -= 2 * MEXC_TICK_SIZE
-    elif full_rmv_balance - INVENTORY_BALANCE < -75_000: # we are short
-        bid_shift += 2 * MEXC_TICK_SIZE
-        ask_shift += 2 * MEXC_TICK_SIZE
-    elif full_rmv_balance - INVENTORY_BALANCE > 150_000:
+    if full_rmv_balance - INVENTORY_BALANCE > 150_000:
         ask_shift -= 3 * MEXC_TICK_SIZE
         bid_shift -= 3 * MEXC_TICK_SIZE
     elif full_rmv_balance - INVENTORY_BALANCE < -150_000:
         ask_shift += 3 * MEXC_TICK_SIZE
         bid_shift += 3 * MEXC_TICK_SIZE
+    elif full_rmv_balance - INVENTORY_BALANCE > 75_000:  # we are long
+        ask_shift -= 2 * MEXC_TICK_SIZE
+        bid_shift -= 2 * MEXC_TICK_SIZE
+    elif full_rmv_balance - INVENTORY_BALANCE < -75_000:  # we are short
+        bid_shift += 2 * MEXC_TICK_SIZE
+        ask_shift += 2 * MEXC_TICK_SIZE
+    elif full_rmv_balance - INVENTORY_BALANCE > 37_500: # we are long
+        ask_shift -= MEXC_TICK_SIZE
+        bid_shift -= MEXC_TICK_SIZE
+    elif full_rmv_balance - INVENTORY_BALANCE < -37_500: # we are short
+        bid_shift += MEXC_TICK_SIZE
+        ask_shift += MEXC_TICK_SIZE
+
+
 
 
     if len(mexc_orderbook.asks) == 0 or len(mexc_orderbook.bids) == 0 or len(kucoin_orderbook.asks) == 0 or len(kucoin_orderbook.bids) == 0:
